861ff3a84ae8b3921a9a98ac062d6465004a32c8
Ivan <istropolitan@users.noreply.github.com>
2015-07-29 19:55:41 +0100

    Added an option of a detail view

--- src/analysis.py
+++ src/analysis.py
@@ -3,13 +3,15 @@ import sys
 import time
 
 class FundCategory:
-  def __init__(self, name):
+  def __init__(self, name, number):
     self.name = name
+    self.number = number
     self.funds = {}
 
 class Fund:
-  def __init__(self, name):
+  def __init__(self, name, number):
     self.name = name
+    self.number = number
     self.data = []
 
   def update(self, values):
@@ -23,7 +25,8 @@ class DataPoint:
 fund_categories = {}
 data_dir = 'data'
 
-def read_line(line):
+def process_line(line):
+  line = line.strip()
   values = line.split(';')
   if values[1] == '':
     return True, values[0]
@@ -32,7 +35,7 @@ def read_line(line):
 def process_values(dates, values):
   data = []
   for d, v in zip(dates, values):
-    dt = time.strptime(d.strip(), '%d.%m.%Y')
+    dt = time.strptime(d, '%d.%m.%Y')
     data.append(DataPoint(dt, v))
   return data
 
@@ -41,6 +44,7 @@ def load_file(file_name):
     pfp_file.readline()
     
     date_line = pfp_file.readline()
+    date_line = date_line.strip()
     dates = date_line.split(';')[1:]
 
     fund_category = 'unknown'
@@ -48,33 +52,80 @@ def load_file(file_name):
       line = pfp_file.readline()
       if not line: break
       
-      is_header, data = read_line(line)
+      is_header, data = process_line(line)
       if is_header:
         fund_category = data
         if fund_category not in fund_categories:
-          fund_categories[fund_category] = FundCategory(fund_category)
+          fund_categories[fund_category] = \
+            FundCategory(fund_category, len(fund_categories) + 1)
       else:
         fund_name = data[0]
         values = data[1:]
         fund_group = fund_categories[fund_category]
         if fund_name not in fund_group.funds:
-          fund_group.funds[fund_name] = Fund(fund_name)
+          fund_group.funds[fund_name] = \
+            Fund(fund_name, len(fund_group.funds) + 1)
         fund = fund_group.funds[fund_name]
         values = process_values(dates, values)
         fund.update(values)
 
 def list_data():
   for fund_category in fund_categories:
-    print(fund_category)
-    for fund in fund_categories[fund_category].funds:
-      fund = fund_categories[fund_category].funds[fund]
-      print('* ' + fund.name + ' (' + str(len(fund.data)) + ' records)')
+    fund_category = fund_categories[fund_category]
+    print(str(fund_category.number) + ': ' + fund_category.name)
+    for fund in fund_category.funds:
+      fund = fund_category.funds[fund]
+      print('* ' + str(fund.number) + ': ' + fund.name
+        + ' (' + str(len(fund.data)) + ' records, first '
+        + time.strftime("%d/%m/%Y", fund.data[0].date) + ', last '
+        + time.strftime("%d/%m/%Y", fund.data[-1].date)  + ')')
     print()
+
+def show_detail(fund_category, fund):
+  print(fund_category.name + ' - ' + fund.name)
+  for day in fund.data:
+    print(time.strftime('%Y-%m-%d', day.date), end='')
+    print(': ', end='')
+    print(day.value, end='')
+    print()
+  print()
+
+def detail():
+  print('Choose a fund category')
+  line = sys.stdin.readline().strip()
+  fund_category_id = int(line)
+  for name, fund_category in fund_categories.items():
+    if fund_category.number == fund_category_id:
+      fund_category = fund_category
+      break
+  
+  print('Choose a fund')
+  line = sys.stdin.readline().strip()
+  fund_id = int(line)
+  for name, fund in fund_category.funds.items():
+    if fund.number == fund_id:
+      break
+
+  show_detail(fund_category, fund)
+
+def menu():
+  print('Choose option:\n'
+        ' d: detail\n'
+        ' q: quit')
 
 pfp_files = os.listdir(data_dir)
 for pfp_file in pfp_files:
   load_file(os.path.join(data_dir, pfp_file))
 list_data()
 
-print('Done')
+menu()
+for cmd in sys.stdin:
+  cmd = cmd.strip()
+  if cmd == 'd':
+    detail()
+  elif cmd == 'q':
+    break
+  menu()
+
+print('Goodbye!')
 
